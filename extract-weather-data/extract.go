c11d68c7a1fbc442cd280846e5e0a5c2cf5e201d
shiftleino <>
2022-07-15 23:18:07 +0300

    Add oauth2 functionality

--- extract-weather-data/extract.go
+++ extract-weather-data/extract.go
@@ -1,12 +1,13 @@
 package extractor
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -17,6 +18,7 @@ const getAccessTokenReq = "oauth2/token"
 const getStationDataReq = "api/getstationsdata?get_favorites=false"
 
 func getAccessToken() string {
+	fmt.Println("Requesting Access Token...")
 	var seachUrl string = baseUrl + getAccessTokenReq
 	godotenv.Load(".env")
 	var clientSecret string = os.Getenv("CLIENT_SECRET")
@@ -24,33 +26,46 @@ func getAccessToken() string {
 	var username string = os.Getenv("USERNAME")
 	var password string = os.Getenv("PASSWORD")
 
-	postBodyBytes, _ := json.Marshal(map[string]string{
-		"grant_type":    "password",
-		"client_id":     clientId,
-		"client_secret": clientSecret,
-		"username":      username,
-		"password":      password,
-		"scope":         "read_station",
-	})
-	postBody := bytes.NewBuffer(postBodyBytes)
-	res, err := http.Post(seachUrl, "application/x-www-form-urlencoded", postBody)
+	urlParameters := url.Values{}
+	urlParameters.Set("grant_type", "password")
+	urlParameters.Set("client_id", clientId)
+	urlParameters.Set("client_secret", clientSecret)
+	urlParameters.Set("username", username)
+	urlParameters.Set("password", password)
+	urlParameters.Set("scope", "read_station")
+	postBody := strings.NewReader(urlParameters.Encode())
+	req, err := http.NewRequest(http.MethodPost, seachUrl, postBody)
+	if err != nil {
+		fmt.Printf("Error when creating request: %s\n", err)
+		os.Exit(1)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+	res, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error when sending request: %s\n", err)
 		os.Exit(1)
 	}
 	defer res.Body.Close()
+	fmt.Printf("Got Response: %d\n", res.StatusCode)
+
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Printf("Error when reading response body: %s\n", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(resBody))
-
-	return "Hello World"
+	var resBodyContent string = string(resBody)
+	var resData map[string]interface{}
+	json.Unmarshal([]byte(resBodyContent), &resData)
+	accessToken := fmt.Sprint(resData["access_token"])
+	return accessToken
 }
 
 func GetStationData() string {
+	fmt.Println("Requesting Weather Station Data...")
 	var searchUrl string = baseUrl + getStationDataReq
 	req, err := http.NewRequest(http.MethodGet, searchUrl, nil)
 	if err != nil {
